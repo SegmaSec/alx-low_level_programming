58f001856994043e983047680c634ffd5c9ac7eb
Its-Sn1p3r <technoth987@gmail.com>
2023-07-14 18:12:54 -0400

    Welcome Variables in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - 0-positive_or_negative.c
+ * Return: 0
+*/
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-    if ( n == 0 )
-    {
-        printf("%d is zero\n",n);
-    }else if ( n > 0 )
-    {
-        pritnf("%d is positive\n".n);
-    }else
-    {
-        printf("%d is negative\n");
-        return (0);
-    }
-	return (0);
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+printf("%d is negative\n", n);
+return (0);
 }
