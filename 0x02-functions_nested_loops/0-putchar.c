63bc73b4bd467f7410dee1c1f65a856fcd1858f7
Its-Sn1p3r <technoth987@gmail.com>
2023-07-18 15:41:52 +0100

    Welcome To C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - 0-putchar.c
+ * Return: 0
+*/
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return (0);
+}
