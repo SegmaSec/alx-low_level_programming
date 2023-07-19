6b0183207292a99128eb8261fdc215e800ee2edd
Its-Sn1p3r <technoth987@gmail.com>
2023-07-19 12:37:08 +0100

    Welcome To C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * @c: character to check
+ * Return:0
+ */
+int _islower(int c)
+{
+if (c < 123 && c > 96)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
