1671ae7ee84bd13288bf28ba343b9d031183d677
Its-Sn1p3r <technoth987@gmail.com>
2023-07-19 12:57:57 +0100

    Welcome To C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: character to check
+ * Return:0
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int _putchar(char ch);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
