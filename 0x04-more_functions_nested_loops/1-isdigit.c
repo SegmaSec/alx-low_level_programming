61ab661e6dbfc3f948630f7c97fa7ebf685e24a5
Its-Sn1p3r <technoth987@gmail.com>
2023-07-20 12:36:37 +0100

    Welcome Function More in C

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - checks lowercase
+ * @c: is the variable
+ * Return: 1 if number if not alpha
+ */
+int _isdigit(int c)
+{
+if (c > 47 && c < 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
