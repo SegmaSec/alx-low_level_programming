b78f30e7b5cd723c5e28d257a52b287a75312289
Its-Sn1p3r <technoth987@gmail.com>
2023-07-20 13:40:25 +0100

    Welcome Function More in C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 int sec;
 int sec1;
-for (sec1 = 0; sec1 < 11; sec1++)
+for (sec1 = 0; sec1 < 10; sec1++)
 {
-for (sec = 0; sec < 15; sec++)
+for (sec = 0; sec <= 14; sec++)
 {
 if (sec > 9)
 {
