02acfa902c9817a409f96d73caa6ebee67a6c3e2
Its-Sn1p3r <technoth987@gmail.com>
2023-07-20 12:11:55 +0100

    Welcome Function More in C

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks lowercase
+ * @c: is the variable
+ * Return: 1 if uppercase if not 0
+ */
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
