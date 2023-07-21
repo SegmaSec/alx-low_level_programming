355640b46aad7f3f986e4b3bb342ce6f177b7bdf
Its-Sn1p3r <technoth987@gmail.com>
2023-07-21 09:41:59 +0100

    Welcome Function More in C

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - checks lowercase
+ * @size: is a variable
+ * Return:
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 0; i < size; i++)
+{
+for (j = size - i; j >= 1; j--)
+{
+_putchar(' ');
+}
+for (j = 0; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
